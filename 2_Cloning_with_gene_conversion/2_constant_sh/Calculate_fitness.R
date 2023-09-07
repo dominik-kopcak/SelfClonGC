adfafd2d4200392e089192506598922c948cbdb6
Dominik Kopčak <s1909657@ed.ac.uk>
2023-09-07 09:33:37 +0100

    preliminary 2_1 upload

--- 2_Cloning_with_gene_conversion/2_constant_sh/Calculate_fitness.R
+++ 2_Cloning_with_gene_conversion/2_constant_sh/Calculate_fitness.R
@@ -1,104 +1,69 @@
 library(dplyr)
 
-Uniparenting_rates <- c(0.00, 0.20, 0.40, 0.60, 0.80, 0.95, 0.99, 1.00)
-N_uniparenting_rates = length(Uniparenting_rates)
-
 h_values = c(0.2, 0.5)
 N_h_values = length(h_values)
 
 s_values = c(0.01)
 N_s_values = length(s_values)
 
+gamma_values = c(10**seq(-4, -10, -2))
+N_gamma_values = length(gamma_values)
+
+lambda_values = c(100)
+N_lambda_values = length(lambda_values)
+
 N_replicates = 5
-Max_time=5000
+Max_time=10000
 
-Mean_fitness_list <- list.files(pattern="Wm_")
-Variance_fitness_list <- list.files(pattern="Wv_")
+Mean_fitness_list_GC <- list.files(pattern="Wm_")
+Variance_fitness_list_GC <- list.files(pattern="Wv_")
 
-Mean_fitness_list_selfing <- Mean_fitness_list[grepl("selfing", Mean_fitness_list)]
-Variance_fitness_list_selfing <- Variance_fitness_list[grepl("selfing", Variance_fitness_list)]
+#print(gamma_values)
+#print(Mean_fitness_list_GC)
 
-Mean_fitness_list_cloning <- Mean_fitness_list[grepl("cloning", Mean_fitness_list)]
-Variance_fitness_list_cloning <- Variance_fitness_list[grepl("cloning", Variance_fitness_list)]
+Selection_coefficient <- rep(s_values, each = N_h_values*N_gamma_values*N_lambda_values*Max_time*N_replicates)
+Dominance_coefficient <- rep(rep(h_values, each = N_gamma_values*N_lambda_values*Max_time*N_replicates), times = N_s_values)
+GC_rate <- rep(rep(gamma_values, each = N_lambda_values*Max_time*N_replicates), times = N_s_values*N_h_values)
+Mean_tract_length <- rep(rep(lambda_values, each = Max_time*N_replicates), times = N_s_values*N_h_values*N_gamma_values)
+Replicate <- rep(rep(c(1:N_replicates), each = Max_time), times = N_s_values*N_h_values*N_gamma_values*N_lambda_values)
+Time <- rep(c(1:Max_time), times = N_s_values*N_h_values*N_gamma_values*N_lambda_values*N_replicates)
 
+Mean_fitness_GC <-rep(c(0), each=N_s_values*N_h_values*N_gamma_values*N_lambda_values*Max_time*N_replicates)
 
-Selection_coefficient <- rep(s_values, each = N_h_values*N_uniparenting_rates*Max_time*N_replicates)
-Dominance_coefficient <- rep(rep(h_values, each = N_uniparenting_rates*Max_time*N_replicates), times = N_s_values)
-Uniparenting_rate <- rep(rep(Uniparenting_rates, each = Max_time*N_replicates), times = N_s_values*N_h_values)
-Replicate <- rep(rep(c(1:N_replicates), each = Max_time), times = N_s_values*N_h_values*N_uniparenting_rates)
-Time <- rep(c(1:Max_time), times = N_s_values*N_h_values*N_uniparenting_rates*N_replicates)
-
-Mean_fitness_selfing <-rep(c(0), each=N_s_values*N_h_values*N_uniparenting_rates*Max_time*N_replicates)
-Mean_fitness_cloning <-rep(c(0), each=N_s_values*N_h_values*N_uniparenting_rates*Max_time*N_replicates)
-
-Variance_fitness_selfing <-rep(c(0), each=N_s_values*N_h_values*N_uniparenting_rates*Max_time*N_replicates)
-Variance_fitness_cloning <-rep(c(0), each=N_s_values*N_h_values*N_uniparenting_rates*Max_time*N_replicates)
+Variance_fitness_GC <-rep(c(0), each=N_s_values*N_h_values*N_gamma_values*N_lambda_values*Max_time*N_replicates)
 
 i=0
-for (w_list in Mean_fitness_list_selfing){
+for (w_list in Mean_fitness_list_GC){
 	file <- read.csv(w_list, header=FALSE, sep="\t")
 	j=0
 	for (w in file$V1){
 		#print(w)
 		j=j+1
 		#print(i*Max_time+j)
-		Mean_fitness_selfing[i*Max_time+j] <- w	
+		Mean_fitness_GC[i*Max_time+j] <- w	
 	}
 	i=i+1
 }
 
 i=0
-for (w_list in Mean_fitness_list_cloning){
+for (w_list in Variance_fitness_list_GC){
 	file <- read.csv(w_list, header=FALSE, sep="\t")
 	j=0
 	for (w in file$V1){
 		#print(w)
 		j=j+1
 		#print(i*Max_time+j)
-		Mean_fitness_cloning[i*Max_time+j] <- w	
+		Variance_fitness_GC[i*Max_time+j] <- w	
 	}
 	i=i+1
 }
 
-i=0
-for (w_list in Variance_fitness_list_selfing){
-	file <- read.csv(w_list, header=FALSE, sep="\t")
-	j=0
-	for (w in file$V1){
-		#print(w)
-		j=j+1
-		#print(i*Max_time+j)
-		Variance_fitness_selfing[i*Max_time+j] <- w	
-	}
-	i=i+1
-}
-
-i=0
-for (w_list in Variance_fitness_list_cloning){
-	file <- read.csv(w_list, header=FALSE, sep="\t")
-	j=0
-	for (w in file$V1){
-		#print(w)
-		j=j+1
-		#print(i*Max_time+j)
-		Variance_fitness_cloning[i*Max_time+j] <- w	
-	}
-	i=i+1
-}
+Fitness_GC <- data.frame(Selection_coefficient, Dominance_coefficient, GC_rate, Mean_tract_length, Replicate, Time, Mean_fitness_GC, Variance_fitness_GC)
 
 
-Fitness_selfing <- data.frame(Selection_coefficient, Dominance_coefficient, Uniparenting_rate, Replicate, Time, Mean_fitness_selfing, Variance_fitness_selfing)
-Fitness_cloning <- data.frame(Selection_coefficient, Dominance_coefficient, Uniparenting_rate, Replicate, Time, Mean_fitness_cloning, Variance_fitness_cloning)
+Fitness_GC_stat <- Fitness_GC %>%
+	group_by(Selection_coefficient, Dominance_coefficient, GC_rate, Time) %>%
+	summarize(Expected_mean_fitness = mean(Mean_fitness_GC), Expected_variance_fitness = mean(Variance_fitness_GC),
+		  SD_mean_fitness = sd(Mean_fitness_GC), SD_variance_fitness = sd(Variance_fitness_GC), .groups = "rowwise")
 
-Fitness_selfing_stat <- Fitness_selfing %>%
-	group_by(Selection_coefficient, Dominance_coefficient, Uniparenting_rate, Time) %>%
-	summarize(Expected_mean_fitness = mean(Mean_fitness_selfing), Expected_variance_fitness = mean(Variance_fitness_selfing),
-		  SD_mean_fitness = sd(Mean_fitness_selfing), SD_variance_fitness = sd(Variance_fitness_selfing), .groups = "rowwise")
-
-Fitness_cloning_stat <- Fitness_cloning %>%
-	group_by(Selection_coefficient, Dominance_coefficient, Uniparenting_rate, Time) %>%
-	summarize(Expected_mean_fitness = mean(Mean_fitness_cloning), Expected_variance_fitness = mean(Variance_fitness_cloning),
-		  SD_mean_fitness = sd(Mean_fitness_cloning), SD_variance_fitness = sd(Variance_fitness_cloning), .groups = "rowwise")
-
-write.csv(Fitness_selfing_stat, file="Data_fitness_evolution_selfing.csv")
-write.csv(Fitness_cloning_stat, file="Data_fitness_evolution_cloning.csv")
+write.csv(Fitness_GC_stat, file="Data_fitness_evolution_GC.csv")
--- 2_Cloning_with_gene_conversion/2_constant_sh/Plot_mutation_accumulation.R
+++ 2_Cloning_with_gene_conversion/2_constant_sh/Plot_mutation_accumulation.R
@@ -3,57 +3,34 @@ library(patchwork)
 library(dplyr)
 
 replicate_number=5
-colours <- c("Selfing"="darkolivegreen2","Cloning"="darkolivegreen")
 
-Selfing_recessive_load_all <- read.csv("Data_selfing_recessive_load.csv")
-Selfing_additive_load_all <- read.csv("Data_selfing_additive_load.csv")
-Selfing_fixed_mutations_all <- read.csv("Data_selfing_fixed_mutations.csv")
-
-Cloning_recessive_load_all <- read.csv("Data_cloning_recessive_load.csv")
-Cloning_additive_load_all <- read.csv("Data_cloning_additive_load.csv")
-Cloning_fixed_mutations_all <- read.csv("Data_cloning_fixed_mutations.csv")
+GC_recessive_load_all <- read.csv("Data_GC_recessive_load.csv")
+GC_additive_load_all <- read.csv("Data_GC_additive_load.csv")
+GC_fixed_mutations_all <- read.csv("Data_GC_fixed_mutations.csv")
 
 for (h_coefficient in c(0.2, 0.5)) {
 
-filename_rec_selfing <- Selfing_recessive_load_all %>% filter(Dominance_coefficient == h_coefficient)
-filename_add_selfing <- Selfing_additive_load_all %>% filter(Dominance_coefficient == h_coefficient)
-filename_fix_selfing <- Selfing_fixed_mutations_all %>% filter(Dominance_coefficient == h_coefficient)
-
-Selfing_rec_max <- filename_rec_selfing$Expected_recessive_load+1.96*filename_rec_selfing$SD_recessive_load/sqrt(replicate_number)
-Selfing_rec_min <- filename_rec_selfing$Expected_recessive_load-1.96*filename_rec_selfing$SD_recessive_load/sqrt(replicate_number)
-
-Selfing_add_max <- filename_add_selfing$Expected_additive_load+1.96*filename_add_selfing$SD_additive_load/sqrt(replicate_number)
-Selfing_add_min <- filename_add_selfing$Expected_additive_load-1.96*filename_add_selfing$SD_additive_load/sqrt(replicate_number)
+filename_rec_GC <- GC_recessive_load_all %>% filter(Dominance_coefficient == h_coefficient)
+filename_add_GC <- GC_additive_load_all %>% filter(Dominance_coefficient == h_coefficient)
+filename_fix_GC <- GC_fixed_mutations_all %>% filter(Dominance_coefficient == h_coefficient)
 
 
-filename_rec_cloning <- Cloning_recessive_load_all %>% filter(Dominance_coefficient == h_coefficient)
-filename_add_cloning <- Cloning_additive_load_all %>% filter(Dominance_coefficient == h_coefficient)
-filename_fix_cloning <- Cloning_fixed_mutations_all %>% filter(Dominance_coefficient == h_coefficient)
+GC_rec_max <- filename_rec_GC$Expected_recessive_load+1.96*filename_rec_GC$SD_recessive_load/sqrt(replicate_number)
+GC_rec_min <- filename_rec_GC$Expected_recessive_load-1.96*filename_rec_GC$SD_recessive_load/sqrt(replicate_number)
 
-
-Cloning_rec_max <- filename_rec_cloning$Expected_recessive_load+1.96*filename_rec_cloning$SD_recessive_load/sqrt(replicate_number)
-Cloning_rec_min <- filename_rec_cloning$Expected_recessive_load-1.96*filename_rec_cloning$SD_recessive_load/sqrt(replicate_number)
-
-Cloning_add_max <- filename_add_cloning$Expected_additive_load+1.96*filename_add_cloning$SD_additive_load/sqrt(replicate_number)
-Cloning_add_min <- filename_add_cloning$Expected_additive_load-1.96*filename_add_cloning$SD_additive_load/sqrt(replicate_number)
-
+GC_add_max <- filename_add_GC$Expected_additive_load+1.96*filename_add_GC$SD_additive_load/sqrt(replicate_number)
+GC_add_min <- filename_add_GC$Expected_additive_load-1.96*filename_add_GC$SD_additive_load/sqrt(replicate_number)
 
 Plot_recessive <- ggplot()+
-	geom_point(data = filename_rec_selfing, aes(x = Uniparenting_rate, y = Expected_recessive_load), color = "darkolivegreen2", size=3, shape=15)+
-	geom_linerange(data = filename_rec_selfing, 
-			aes(x=Uniparenting_rate, 
-			    ymax=Selfing_rec_max, 
-			    ymin=Selfing_rec_min), color="darkolivegreen2", size=1)+
- 
- 	geom_point(data=filename_rec_cloning, aes(x=Uniparenting_rate, y=Expected_recessive_load), color="darkolivegreen", size=3, shape=15)+
-	geom_linerange(data=filename_rec_cloning, 
-			aes(x=Uniparenting_rate, 
-			    ymax=Cloning_rec_max, 
-			    ymin=Cloning_rec_min), color="darkolivegreen", size=1)+
+ 	geom_point(data=filename_rec_GC, aes(x=GC_rate, y=Expected_recessive_load), size=3, shape=15)+
+	geom_linerange(data=filename_rec_GC, 
+			aes(x=GC_rate, 
+			    ymax=GC_rec_max, 
+			    ymin=GC_rec_min), size=1)+
 	
 	labs(y="Homozygous genotypes",
 	     x="Rate of uniparental reproduction",
-	     title="Accumulation of mutations\nin populations with uniparental reproduction",
+	     title="Accumulation of mutations\nin completely clonal populations\nwith gene conversion",
 	     subtitle="Recessive load")+
 	 theme(panel.grid.major=element_blank(),
     	      panel.grid.minor=element_blank(),
@@ -69,28 +46,21 @@ Plot_recessive <- ggplot()+
 	      axis.text.y=element_text(size=15),
 	      
 	      legend.position="none")+
-	scale_colour_manual(values = colours)+
-	scale_x_continuous(breaks=seq(0, 1, 0.2))
+	      
+  	scale_x_log10(breaks=c(10**seq(-10, -4, 2)))
 		   
 Plot_additive <- ggplot()+
-	geom_point(data=filename_add_selfing, aes(x=Uniparenting_rate, y=Expected_additive_load/1000, colour="Selfing"), size=3, shape=15)+
-	geom_linerange(data=filename_add_selfing, 
-			aes(x=Uniparenting_rate, 
-			    ymax=Selfing_add_max/1000, 
-			    ymin=Selfing_add_min/1000), color="darkolivegreen2", size=1, 
-			key_glyph = draw_key_rect)+
-	
-	geom_point(data=filename_add_cloning, aes(x=Uniparenting_rate, y=Expected_additive_load/1000, colour="Cloning"), size=3, shape=15)+
-	geom_linerange(data=filename_add_cloning, 
-			aes(x=Uniparenting_rate, 
-			    ymax=Cloning_add_max/1000, 
-			    ymin=Cloning_add_min/1000), color="darkolivegreen", size=1, 
+	geom_point(data=filename_add_GC, aes(x=GC_rate, y=Expected_additive_load/1000), size=3, shape=15)+
+	geom_linerange(data=filename_add_GC, 
+			aes(x=GC_rate, 
+			    ymax=GC_add_max/1000, 
+			    ymin=GC_add_min/1000), size=1, 
 			key_glyph = draw_key_rect)+
 	
 	labs(subtitle="Additive load",
-	     x="Rate of uniparental reproduction",
+	     x="Rate of gene conversion",
 	     y="Total mutation count (x 1000)",
-	     caption=paste("s = 0.01, h = ", h_coefficient, ", mutation rate = 4E-9, population size = 5 000\ngenome is 1 chromosome with size 25 Mbp, recombination rate = 4E-8\nnumber of replicates = 5, sample size = 50, run for 5 000 generations", sep=""),
+	     caption=paste("s = 0.01, h = ", h_coefficient, ", mutation rate = 4E-9, population size = 5 000\ngenome is 1 chromosome with size 25 Mbp, mean GC tract length = 100 bp\nnumber of replicates = 5, sample size = 50, run for 10 000 generations", sep=""),
 	     colour="Reproduction: ")+
 	theme(panel.grid.major=element_blank(),
     	      panel.grid.minor=element_blank(),
@@ -108,14 +78,12 @@ Plot_additive <- ggplot()+
 	      legend.position=c(0.12, 2.1),
 	      legend.background=element_rect(colour="black"))+
 	      
-	scale_colour_manual(values = colours)+
-	scale_x_continuous(breaks=seq(0, 1, 0.2))
-
+  	scale_x_log10(breaks=c(10**seq(-10, -4, 2)))
+  	
 Plot <- Plot_recessive/Plot_additive
 
-plot_name <- paste("Figure_Mutation_accumulation_selfing_vs_cloning_", h_coefficient, ".png",sep="")
+plot_name <- paste("Figure_Mutation_accumulation_GC_", h_coefficient, ".png",sep="")
 ggsave(filename=plot_name,
 	plot=Plot,
 	dpi=600)
-	
 }

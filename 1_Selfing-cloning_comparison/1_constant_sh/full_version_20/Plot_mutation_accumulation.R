01d1925b59d1fcfbcec53491bce51d5185301372
Dominik Kopčak <s1909657@ed.ac.uk>
2023-11-17 09:05:16 +0000

    Selfing-cloning constant_sh plots design update + zoomed versions

--- 1_Selfing-cloning_comparison/1_constant_sh/full_version_20/Plot_mutation_accumulation.R
+++ 1_Selfing-cloning_comparison/1_constant_sh/full_version_20/Plot_mutation_accumulation.R
@@ -53,8 +53,7 @@ Plot_recessive <- ggplot()+
 	
 	labs(y="Homozygous genotypes",
 	     x="Rate of uniparental reproduction",
-	     title="Accumulation of mutations\nin populations with uniparental reproduction",
-	     subtitle="Recessive load")+
+	     title="Accumulation of mutations\nin populations with uniparental reproduction")+
 	 theme(panel.grid.major=element_blank(),
     	      panel.grid.minor=element_blank(),
     	      panel.background=element_blank(),
@@ -87,10 +86,49 @@ Plot_additive <- ggplot()+
 			    ymin=Cloning_add_min/1000), color="darkolivegreen", size=1, 
 			key_glyph = draw_key_rect)+
 	
-	labs(subtitle="Additive load",
-	     x="Rate of uniparental reproduction",
-	     y="Total mutation count (x 1000)",
-	     caption=paste("s = 0.01, h = ", h_coefficient, ", mutation rate = 4E-9, population size = 5 000\ngenome is 1 chromosome with size 25 Mbp, recombination rate = 4E-8\nnumber of replicates = ", replicate_number,", sample size = 50, run for 5 000 generations", sep=""),
+	labs(x="Rate of uniparental reproduction",
+	     y="Total mutation count\n(x 1000)",
+	     caption=paste("s = 0.01, h = ", h_coefficient, ", mutation rate = 4E-9, population size = 5 000\ngenome is 1 chromosome with size 25 Mbp, recombination rate = 4E-8\nnumber of replicates = ", replicate_number,", sample size = 50, run for 20 000 generations", sep=""),
+	     colour="Reproduction: ")+
+	theme(panel.grid.major=element_blank(),
+    	      panel.grid.minor=element_blank(),
+    	      panel.background=element_blank(),
+    	      axis.line = element_line(color="black"),
+    	      
+	      plot.caption=element_text(size=10, hjust=0.5),
+	      plot.subtitle=element_text(size=15, hjust=1, face="bold"), 
+	      axis.title.y=element_text(size=15),
+	      axis.title.x=element_text(size=15),
+	      
+	      axis.text.x=element_text(size=0),
+	      axis.text.y=element_text(size=15),
+	      
+	      legend.position=c(0.12, 1.9),
+	      legend.key = element_rect(fill = "white"))+
+
+	guides(colour = guide_legend(override.aes = list(size = 8)))+
+		
+	scale_colour_manual(values = colours)+
+	scale_x_continuous(breaks=seq(0, 1, 0.2))
+
+Plot_additive_2 <- ggplot()+
+	geom_point(data=filename_add_selfing, aes(x=Uniparenting_rate, y=Expected_additive_load/1000, colour="Selfing"), size=3, shape=15)+
+	geom_linerange(data=filename_add_selfing, 
+			aes(x=Uniparenting_rate, 
+			    ymax=Selfing_add_max/1000, 
+			    ymin=Selfing_add_min/1000), color="darkolivegreen2", size=1, 
+			key_glyph = draw_key_rect)+
+	
+	geom_point(data=filename_add_cloning, aes(x=Uniparenting_rate, y=Expected_additive_load/1000, colour="Cloning"), size=3, shape=15)+
+	geom_linerange(data=filename_add_cloning, 
+			aes(x=Uniparenting_rate, 
+			    ymax=Cloning_add_max/1000, 
+			    ymin=Cloning_add_min/1000), color="darkolivegreen", size=1, 
+			key_glyph = draw_key_rect)+
+	
+	labs(x="Rate of uniparental reproduction",
+	     y="Total mutation count\n(x 1000)",
+	     caption=paste("s = 0.01, h = ", h_coefficient, ", mutation rate = 4E-9, population size = 5 000\ngenome is 1 chromosome with size 25 Mbp, recombination rate = 4E-8\nnumber of replicates = ", replicate_number,", sample size = 50, run for 20 000 generations", sep=""),
 	     colour="Reproduction: ")+
 	theme(panel.grid.major=element_blank(),
     	      panel.grid.minor=element_blank(),
@@ -105,17 +143,25 @@ Plot_additive <- ggplot()+
 	      axis.text.x=element_text(size=15),
 	      axis.text.y=element_text(size=15),
 	      
-	      legend.position=c(0.12, 2.1),
-	      legend.background=element_rect(colour="black"))+
-	      
+	      legend.position=c(0.12, 1.9),
+	      legend.key = element_rect(fill = "white"))+
+
+	guides(colour = guide_legend(override.aes = list(size = 8)))+
+		
 	scale_colour_manual(values = colours)+
-	scale_x_continuous(breaks=seq(0, 1, 0.2))
+	scale_x_continuous(breaks=seq(0, 1, 0.2))+
+	scale_y_continuous(limits=c(0, 5))
 
 Plot <- Plot_recessive/Plot_additive
+Plot_2 <- Plot_recessive/Plot_additive_2
 
 plot_name <- paste("Figure_Mutation_accumulation_selfing_vs_cloning_", h_coefficient, ".png",sep="")
 ggsave(filename=plot_name,
 	plot=Plot,
 	dpi=600)
-	
+
+plot_name_2 <- paste("Figure_Mutation_accumulation_selfing_vs_cloning_", h_coefficient, "v2.png",sep="")
+ggsave(filename=plot_name_2,
+	plot=Plot_2,
+	dpi=600)
 }

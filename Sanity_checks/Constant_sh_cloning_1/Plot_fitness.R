60e96d2226da2e4550ca2bad562a69b6781a2015
Dominik Kopčak <s1909657@ed.ac.uk>
2023-11-22 21:30:36 +0000

    fitness evolution comparison WF and nonWF

--- Sanity_checks/Constant_sh_cloning_1/Plot_fitness.R
+++ Sanity_checks/Constant_sh_cloning_1/Plot_fitness.R
@@ -5,49 +5,77 @@ h_values = c(0.2, 0.5)
 
 Max_time=20000
 
-N_replicates_cloning = 100
-N_replicates_cloning_GC = 20
-colours <- c("Cloning"="red","Cloning+GC"="lightblue")
+N_replicates = 100
+colours <- c("Cloning"="red","Cloning+GC_1E-10"="#00BFFF", "Cloning+GC_1E-11"="#00B2EE", "Cloning+GC_1E-12"="#009ACD", "Cloning+GC_1E-13"="#00688B")
 
 Fitness_cloning <- read.csv("/home/dvojkvietok/Documents/SlefingCloningGC/1_Selfing-cloning_comparison/1_constant_sh/full_version_20/Data_fitness_evolution_cloning.csv")
 Fitness_cloning_GC <- read.csv("/home/dvojkvietok/Documents/SlefingCloningGC/2_Cloning_with_gene_conversion/2_constant_sh/lambda_100/Data_fitness_evolution_GC.csv")
-
+Fitness_cloning_GC_extra_small <- read.csv("/home/dvojkvietok/Documents/SlefingCloningGC/2_Cloning_with_gene_conversion/2_constant_sh/lambda_100/Data_fitness_evolution_GC_extra_small.csv")
 
 for (h_coefficient in h_values){
 
-	fitness_cloning_subset_0 <- Fitness_cloning %>%
+	fitness_cloning <- Fitness_cloning %>%
 		filter(Dominance_coefficient == h_coefficient,
 			Uniparenting_rate == 1.00)
 	
-	fitness_cloning_GC_subset_0 <- Fitness_cloning_GC %>%
+	fitness_cloning_GC <- Fitness_cloning_GC %>%
 		 filter(Dominance_coefficient == h_coefficient,
 			GC_rate == 10**(-10))
 
-
-	Plot_mean_ <- ggplot(data=fitness_cloning_subset_0, aes(x=Time, y=Expected_mean_fitness), size=1) +
-
-			         geom_line(data=fitness_cloning_subset_0, aes(x=Time, y=Expected_mean_fitness), colour = "red") +
-				 geom_ribbon(data=fitness_cloning_subset_0, aes(x=Time, ymin=Expected_mean_fitness - 1.96*SD_mean_fitness/N_replicates_cloning, ymax=Expected_mean_fitness + 1.96*SD_mean_fitness/N_replicates_cloning, colour = "Cloning"), alpha = 0.5) +
+	fitness_cloning_GC_extra_small_11 <- Fitness_cloning_GC_extra_small %>%
+		 filter(Dominance_coefficient == h_coefficient,
+			GC_rate == 10**(-11))
+	fitness_cloning_GC_extra_small_12 <- Fitness_cloning_GC_extra_small %>%
+		 filter(Dominance_coefficient == h_coefficient,
+			GC_rate == 10**(-12))
+	fitness_cloning_GC_extra_small_13 <- Fitness_cloning_GC_extra_small %>%
+		 filter(Dominance_coefficient == h_coefficient,
+			GC_rate == 10**(-13))
+			
+	Plot_mean_ <- ggplot() +
+				 geom_line(data=fitness_cloning_GC, aes(x=Time, y=Expected_mean_fitness, colour = "Cloning+GC_1E-10")) +
+				 geom_ribbon(data=fitness_cloning_GC, aes(x=Time, ymin=(Expected_mean_fitness - 1.96*SD_mean_fitness/N_replicates), ymax=(Expected_mean_fitness + 1.96*SD_mean_fitness/N_replicates), fill = "Cloning+GC_1E-10"), alpha = 0.5) +
+	
+				 geom_line(data=fitness_cloning_GC_extra_small_11, aes(x=Time, y=Expected_mean_fitness, colour = "Cloning+GC_1E-11")) +
+				 geom_ribbon(data=fitness_cloning_GC_extra_small_11, aes(x=Time, ymin=(Expected_mean_fitness - 1.96*SD_mean_fitness/N_replicates), ymax=(Expected_mean_fitness + 1.96*SD_mean_fitness/N_replicates), fill = "Cloning+GC_1E-11"), alpha = 0.5) +
+	
+				 geom_line(data=fitness_cloning_GC_extra_small_12, aes(x=Time, y=Expected_mean_fitness, colour = "Cloning+GC_1E-12")) +
+				 geom_ribbon(data=fitness_cloning_GC_extra_small_12, aes(x=Time, ymin=(Expected_mean_fitness - 1.96*SD_mean_fitness/N_replicates), ymax=(Expected_mean_fitness + 1.96*SD_mean_fitness/N_replicates), fill = "Cloning+GC_1E-12"), alpha = 0.5) +
 				 
-				 geom_line(data=fitness_cloning_GC_subset_0, aes(x=Time, y=Expected_mean_fitness), colour = "lightblue", key_glyph = draw_key_rect) +
-				 geom_ribbon(data=fitness_cloning_GC_subset_0, aes(x=Time, ymin=Expected_mean_fitness - 1.96*SD_mean_fitness/N_replicates_cloning, ymax=Expected_mean_fitness + 1.96*SD_mean_fitness/N_replicates_cloning, colour = "Cloning+GC"), alpha = 0.5)
+				 geom_line(data=fitness_cloning_GC_extra_small_13, aes(x=Time, y=Expected_mean_fitness, colour = "Cloning+GC_1E-13")) +
+				 geom_ribbon(data=fitness_cloning_GC_extra_small_13, aes(x=Time, ymin=(Expected_mean_fitness - 1.96*SD_mean_fitness/N_replicates), ymax=(Expected_mean_fitness + 1.96*SD_mean_fitness/N_replicates), fill = "Cloning+GC_1E-13"), alpha = 0.5) +
+				 
+				 geom_line(data=fitness_cloning, aes(x=Time, y=Expected_mean_fitness, colour = "Cloning")) +
+				 geom_ribbon(data=fitness_cloning, aes(x=Time, ymin=(Expected_mean_fitness - 1.96*SD_mean_fitness/N_replicates), ymax=(Expected_mean_fitness + 1.96*SD_mean_fitness/N_replicates), fill = "Cloning"), alpha = 0.5)
 	
-	
-	Plot_variance_ <- ggplot(data=fitness_cloning_GC_subset_0, aes(x=Time, y=Expected_variance_fitness), size=1) +
-
-				     geom_line(data=fitness_cloning_subset_0, aes(x=Time, y=Expected_variance_fitness), colour = "red")+
-				     geom_ribbon(data=fitness_cloning_subset_0, aes(x=Time, ymin=(Expected_variance_fitness - 1.96*SD_variance_fitness/N_replicates_cloning_GC), ymax=(Expected_variance_fitness + 1.96*SD_variance_fitness/N_replicates_cloning_GC), colour = "Cloning"), alpha = 0.5) +
+	Plot_variance_ <- ggplot() +
+				     geom_line(data=fitness_cloning_GC, aes(x=Time, y=Expected_variance_fitness, colour = "Cloning+GC_1E-10"))+
+				     geom_ribbon(data=fitness_cloning_GC, aes(x=Time, ymin=(Expected_variance_fitness - 1.96*SD_variance_fitness/N_replicates), ymax=(Expected_variance_fitness + 1.96*SD_variance_fitness/N_replicates), fill = "Cloning+GC_1E-10"), alpha = 0.5) +
 				     
-				     geom_line(data=fitness_cloning_GC_subset_0, aes(x=Time, y=Expected_variance_fitness), colour = "lightblue", key_glyph = draw_key_rect)+
-				     geom_ribbon(data=fitness_cloning_GC_subset_0, aes(x=Time, ymin=Expected_variance_fitness - 1.96*SD_variance_fitness/N_replicates_cloning_GC, ymax=Expected_variance_fitness + 1.96*SD_variance_fitness/N_replicates_cloning_GC, colour = "Cloning+GC"), alpha = 0.5)
-	
+				     geom_line(data=fitness_cloning_GC_extra_small_11, aes(x=Time, y=Expected_variance_fitness, colour = "Cloning+GC_1E-11"))+
+				     geom_ribbon(data=fitness_cloning_GC_extra_small_11, aes(x=Time, ymin=Expected_variance_fitness - 1.96*SD_variance_fitness/N_replicates, ymax=Expected_variance_fitness + 1.96*SD_variance_fitness/N_replicates, fill = "Cloning+GC_1E-11"), alpha = 0.5) +
+				     
+				     geom_line(data=fitness_cloning_GC_extra_small_12, aes(x=Time, y=Expected_variance_fitness, colour = "Cloning+GC_1E-12"))+
+				     geom_ribbon(data=fitness_cloning_GC_extra_small_12, aes(x=Time, ymin=Expected_variance_fitness - 1.96*SD_variance_fitness/N_replicates, ymax=Expected_variance_fitness + 1.96*SD_variance_fitness/N_replicates, fill = "Cloning+GC_1E-12"), alpha = 0.5) +
+							     
+				     geom_line(data=fitness_cloning_GC_extra_small_13, aes(x=Time, y=Expected_variance_fitness, colour = "Cloning+GC_1E-13"))+
+				     geom_ribbon(data=fitness_cloning_GC_extra_small_13, aes(x=Time, ymin=Expected_variance_fitness - 1.96*SD_variance_fitness/N_replicates, ymax=Expected_variance_fitness + 1.96*SD_variance_fitness/N_replicates, fill = "Cloning+GC_1E-13"), alpha = 0.5) +
+						     
+				     geom_line(data=fitness_cloning, aes(x=Time, y=Expected_variance_fitness, colour = "Cloning"))+
+				     geom_ribbon(data=fitness_cloning, aes(x=Time, ymin=(Expected_variance_fitness - 1.96*SD_variance_fitness/N_replicates), ymax=(Expected_variance_fitness + 1.96*SD_variance_fitness/N_replicates), fill = "Cloning"), alpha = 0.5)
+
+   	
 	Plot_mean_ <- Plot_mean_ +
-		labs(title="Population fitness\nunder  reproduction",
+		labs(title="Comparison of WF and nonWF models",
 		     x="Time",
 		     y="Mean fitness",
-		     caption=paste("s = 0.01, h = ", h_coefficient, ", mutation rate = 4E-9, population size = 5 000\ngenome is 1 chromosome with size 25 Mbp, recombination rate = 4E-8\nnumber of replicates = 100, sample size = 50, run for 5 000 generations", sep=""))+
+		     caption=paste("IFor both simulation sets: s = 0.01, h = ", h_coefficient, ", mutation rate = 4E-9, population size = 5 000\ngenome is 1 chromosome with size 25 Mbp, recombination rate = 4E-8\nnumber of replicates = 100, sample size = 50, run for 20 000 generations", sep=""),
+		     colour="Reproduction: ")+
 	
-		scale_colour_manual(values = colours)+
+		scale_colour_manual(values = colours) +
+		scale_fill_manual(values = colours, guide = "none") + 
+		
+		guides(colour = guide_legend(override.aes = list(size = 10))) +
 	     
 		theme(panel.grid.major=element_blank(),
     	           panel.grid.minor=element_blank(),
@@ -63,18 +91,24 @@ for (h_coefficient in h_values){
 	      
 	    	   axis.text.x=element_text(size=15),
 	    	   axis.text.y=element_text(size=15),
+	    	   
+	    	   legend.position=c(0.8, 0.5),
 	    	   
 	    	   legend.key = element_blank(),
 	    	   legend.key.size = unit(1, "cm"))
 	    	   
 		    	   
 	 Plot_variance_ <- Plot_variance_ +
-		labs(title="Population fitness variance\nunder  reproduction",
+		labs(title="Comparison of WF and nonWF models",
 		     x="Time",
 		     y="Fitness variance",
-		     caption=paste("s = 0.01, h = ", h_coefficient, ", mutation rate = 4E-9, population size = 5 000\ngenome is 1 chromosome with size 25 Mbp, recombination rate = 4E-8\nnumber of replicates = #, sample size = 50, run for # 000 generations", sep=""))+
+		     caption=paste("For both simulation sets: s = 0.01, h = ", h_coefficient, ", mutation rate = 4E-9, population size = 5 000\ngenome is 1 chromosome with size 25 Mbp, recombination rate = 4E-8\nnumber of replicates = 100, sample size = 50, run for 20 000 generations", sep=""),
+		     colour="Reproduction: ")+
 
 		scale_colour_manual(values = colours)+
+		scale_fill_manual(values = colours, guide = "none") + 
+		
+		guides(colour = guide_legend(override.aes = list(size = 10)))+
 	     
 		theme(panel.grid.major=element_blank(),
     	           panel.grid.minor=element_blank(),
@@ -87,9 +121,11 @@ for (h_coefficient in h_values){
 	   	   
 	   	   axis.title.y=element_text(size=15),
 	    	   axis.title.x=element_text(size=15),
-	      
+	         
 	    	   axis.text.x=element_text(size=15),
 	    	   axis.text.y=element_text(size=15),
+	    	   
+	    	   legend.position=c(0.8, 0.5),
 	    	   
 	    	   legend.key = element_blank(),
 	    	   legend.key.size = unit(1, "cm"))

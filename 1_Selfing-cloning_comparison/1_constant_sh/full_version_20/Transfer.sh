fe91e4dc9013ada644e2d1d8eaa6e9b12c8b5c59
Dominik Kopčak <s1909657@ed.ac.uk>
2024-08-11 02:47:44 +0900

    removing unnecessary from 1_1

--- 1_Selfing-cloning_comparison/1_constant_sh/full_version_20/Transfer.sh
+++
@@ -1,3 +0,0 @@
-rsync -avz /scratch/dkopcak/1_Selfing-cloning_comparison/X_* dkopcak@qmaster:/data/hartfield/asex_load/raw/1_Selfing-cloning_comparison/constant_sh
-rsync -avz /scratch/dkopcak/1_Selfing-cloning_comparison/Wm_* dkopcak@qmaster:/data/hartfield/asex_load/raw/1_Selfing-cloning_comparison/constant_sh
-rsync -avz /scratch/dkopcak/1_Selfing-cloning_comparison/Wv_* dkopcak@qmaster:/data/hartfield/asex_load/raw/1_Selfing-cloning_comparison/constant_sh

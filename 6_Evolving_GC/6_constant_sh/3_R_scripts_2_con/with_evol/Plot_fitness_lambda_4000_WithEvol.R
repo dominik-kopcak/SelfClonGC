f41cbf09cfd8e9413ef6d412b2448960946833a8
Matthew Hartfield <m.hartfield@ed.ac.uk>
2026-06-18 16:24:06 +0100

    Updated GC evol fitness plot code

--- 6_Evolving_GC/6_constant_sh/3_R_scripts_2_con/with_evol/Plot_fitness_lambda_4000_WithEvol.R
+++ 6_Evolving_GC/6_constant_sh/3_R_scripts_2_con/with_evol/Plot_fitness_lambda_4000_WithEvol.R
@@ -10,10 +10,14 @@ N_replicates = 20
 
 setwd("/mnt/loki/hartfield/AsexMuts/scripts/SelfClonGC/6_Evolving_GC/6_constant_sh/4_Data_2_con")
 
-Fitness_GC <- read.csv("Data_fitness_evolution_GC_WithEvol.csv")
-Fitness_GC$GC_rate <- as.factor(Fitness_GC$GC_rate)
-Fitness_GC$GC_var <- as.factor(Fitness_GC$GC_var)
+Fitness_GC_in <- read.csv("Data_fitness_evolution_GC_WithEvol.csv")
 
+Fitness_GC <- transform(Fitness_GC_in,
+	GC_var = factor(GC_var, levels=unique(Fitness_GC_in$GC_var), labels=c(expression("Standard Deviation = " ~ 1 %*% 10 ^ -8), expression("Standard Deviation = " ~ 1 %*% 10 ^ -7))))
+Fitness_GC$GC_rate <- as.factor(Fitness_GC$GC_rate)
+
+#Fitness_GC$GC_var <- as.factor(Fitness_GC$GC_var)
+#GC_var_labs <- c("1e-08" = expression(), "1e-07" = expression("Standard Deviation = " ~ 1 %*% 10 ^ -7))
 
 for (h_coefficient in h_values){
 	
@@ -21,9 +25,9 @@ for (h_coefficient in h_values){
 		 filter(Dominance_coefficient == h_coefficient,
 			GC_rate == 0)
 	
-	Plot_mean_GC <- ggplot(data=fitness_GC_subset_0, aes(x=Time, y=Expected_mean_fitness, group=GC_var))
-	Plot_variance_GC <- ggplot(data=fitness_GC_subset_0, aes(x=Time, y=Expected_variance_fitness, group=GC_var))
-	Plot_evol_GC <- ggplot(data=fitness_GC_subset_0, aes(x=Time, y=Expected_GC, group=GC_var))
+	Plot_mean_GC <- ggplot(data=fitness_GC_subset_0, aes(x=Time, y=Expected_mean_fitness))
+	Plot_variance_GC <- ggplot(data=fitness_GC_subset_0, aes(x=Time, y=Expected_variance_fitness))
+	Plot_evol_GC <- ggplot(data=fitness_GC_subset_0, aes(x=Time, y=Expected_GC))
 	
 	for (rate in unique(Fitness_GC$GC_rate)){
 	
@@ -31,29 +35,29 @@ for (h_coefficient in h_values){
 			filter(Dominance_coefficient == h_coefficient,
 				GC_rate == rate)
 		
-		psize <- 0.5
+		psize <- 1
 			
 		Plot_mean_GC <- Plot_mean_GC +
-			geom_line(data=fitness_GC_subset, aes(x=Time, y=Expected_mean_fitness, colour = GC_rate, linetype = GC_var), size = psize) +
-			geom_ribbon(data=fitness_GC_subset, aes(x=Time, ymin=CI_fit_L, ymax=CI_fit_U, fill = GC_rate, alpha = GC_var))
+			geom_line(data=fitness_GC_subset, aes(x=Time, y=Expected_mean_fitness, colour = GC_rate), size = psize) +
+			geom_ribbon(data=fitness_GC_subset, aes(x=Time, ymin=CI_fit_L, ymax=CI_fit_U, fill = GC_rate), alpha = 0.5)
 			
 		Plot_variance_GC <- Plot_variance_GC +
-			geom_line(data=fitness_GC_subset, aes(x=Time, y=Expected_variance_fitness, colour = GC_rate, linetype = GC_var), size = psize) +
-			geom_ribbon(data=fitness_GC_subset, aes(x=Time, ymin=CI_var_L, ymax=CI_var_U, fill = GC_rate, alpha = GC_var))
+			geom_line(data=fitness_GC_subset, aes(x=Time, y=Expected_variance_fitness, colour = GC_rate), size = psize) +
+			geom_ribbon(data=fitness_GC_subset, aes(x=Time, ymin=CI_var_L, ymax=CI_var_U, fill = GC_rate), alpha = 0.5)
 			
 		Plot_evol_GC <- Plot_evol_GC +
-			geom_line(data=fitness_GC_subset, aes(x=Time, y=Expected_GC, colour = GC_rate, linetype = GC_var), size = psize) +
-			geom_ribbon(data=fitness_GC_subset, aes(x=Time, ymin=CI_GC_L, ymax= CI_GC_U, fill = GC_rate, alpha = GC_var))
+			geom_line(data=fitness_GC_subset, aes(x=Time, y=Expected_GC, colour = GC_rate), size = psize) +
+			geom_ribbon(data=fitness_GC_subset, aes(x=Time, ymin=CI_GC_L, ymax= CI_GC_U, fill = GC_rate), alpha = 0.5)
 	
 	}
 	  	   
 	 Plot_mean_GC <- Plot_mean_GC +
 		scale_colour_viridis_d(guide = "none") + 
 		scale_fill_viridis_d(breaks=unique(Fitness_GC$GC_rate), labels=c(expression(4 %*% 10 ^ -7), expression(4 %*% 10 ^ -4), expression(4 %*% 10 ^ -2))) +
-		scale_alpha_manual(breaks = unique(Fitness_GC$GC_var), values = c(0.6, 0.3), guide='none') + 
-		scale_linetype_manual(name = "Standard deviation in mitotic\ngene conversion mutation rates", values = c("solid", "dotted"), labels=c(expression(1 %*% 10 ^ -8), expression(1 %*% 10 ^ -7))) +
-		labs(title="Population Fitness\nUnder Asexual Reproduction with GC",
-		     x="Time",
+#		scale_alpha_manual(breaks = unique(Fitness_GC$GC_var), values = c(0.6, 0.3), guide='none') + 
+#		scale_linetype_manual(name = "Standard deviation in mitotic\ngene conversion mutation rates", values = c("solid", "62"), labels=c(expression(1 %*% 10 ^ -8), expression(1 %*% 10 ^ -7))) +
+		facet_grid(cols = vars(GC_var), labeller = label_parsed) +
+		labs(x="Time",
 		     y="Mean fitness")+
 		guides(fill = guide_legend(title = "Starting gene conversion\nrate per site")) +
 	     
@@ -61,6 +65,9 @@ for (h_coefficient in h_values){
     	           panel.grid.minor=element_blank(),
     	  	   panel.background=element_blank(),
     	   	   axis.line = element_line(color="black"),
+    	   	   
+    	   	   strip.text = element_text(face = "bold", size = rel(2.5)),
+    	   	   panel.spacing = unit(2, "lines"),
     	   	   
 	    	   plot.title=element_text(size=40, hjust=0.5, face="bold"),
 	    	   plot.caption=element_text(size=20, hjust=0.5),
@@ -78,10 +85,10 @@ for (h_coefficient in h_values){
 	  Plot_variance_GC <- Plot_variance_GC +
 		scale_colour_viridis_d(guide = "none") + 
 		scale_fill_viridis_d(breaks=unique(Fitness_GC$GC_rate), labels=c(expression(4 %*% 10 ^ -7), expression(4 %*% 10 ^ -4), expression(4 %*% 10 ^ -2))) + 
-		scale_alpha_manual(breaks = unique(Fitness_GC$GC_var), values = c(0.6, 0.3), guide='none') + 
-		scale_linetype_manual(name = "Standard deviation in mitotic\ngene conversion mutation rates", values = c("solid", "dotted"), labels=c(expression(1 %*% 10 ^ -8), expression(1 %*% 10 ^ -7))) +
-		labs(title="Population Fitness Variance\nUnder Asexual Reproduction with GC",
-		     x="Time",
+#		scale_alpha_manual(breaks = unique(Fitness_GC$GC_var), values = c(0.6, 0.3), guide='none') + 
+#		scale_linetype_manual(name = "Standard deviation in mitotic\ngene conversion mutation rates", values = c("solid", "62"), labels=c(expression(1 %*% 10 ^ -8), expression(1 %*% 10 ^ -7))) +
+		facet_grid(cols = vars(GC_var), labeller = label_parsed) +
+		labs(x="Time",
 		     y="Fitness variance")+
 		guides(fill = guide_legend(title = "Starting gene conversion\nrate per site")) +
 	     
@@ -89,6 +96,9 @@ for (h_coefficient in h_values){
     	           panel.grid.minor=element_blank(),
     	  	   panel.background=element_blank(),
     	   	   axis.line = element_line(color="black"),
+    	   	   
+       	   	   strip.text = element_text(face = "bold", size = rel(2.5)),
+       	   	   panel.spacing = unit(2, "lines"),
     	   	   
 	    	   plot.title=element_text(size=40, hjust=0.5, face="bold"),
 	    	   plot.caption=element_text(size=20, hjust=0.5),
@@ -106,18 +116,21 @@ for (h_coefficient in h_values){
 	  Plot_evol_GC <- Plot_evol_GC +
   	 	scale_y_log10() +
 		scale_colour_viridis_d(guide = "none") + 
-		scale_fill_viridis_d(breaks=unique(Fitness_GC$GC_rate), labels=c(expression(4 %*% 10 ^ -7), expression(4 %*% 10 ^ -4), expression(4 %*% 10 ^ -2))) +
-		scale_alpha_manual(breaks = unique(Fitness_GC$GC_var), values = c(0.6, 0.3), guide='none') + 
-		scale_linetype_manual(name = "Standard deviation in mitotic\ngene conversion mutation rates", values = c("solid", "dotted"), labels=c(expression(1 %*% 10 ^ -8), expression(1 %*% 10 ^ -7))) +
-		labs(title="Evolving GC rate\nUnder Asexual Reproduction",
-		     x="Time",
+		scale_fill_viridis_d(breaks=unique(Fitness_GC$GC_rate), guide = "none") +
+#		scale_alpha_manual(breaks = unique(Fitness_GC$GC_var), values = c(0.6, 0.3), guide='none') + 
+#		scale_linetype_manual(name = "Standard deviation in mitotic\ngene conversion mutation rates", values = c("solid", "62"), labels=c(expression(1 %*% 10 ^ -8), expression(1 %*% 10 ^ -7))) +
+		facet_grid(cols = vars(GC_var), labeller = label_parsed) +
+		labs(x="Time",
 		     y="Mitotic gene conversion rate")+
-		guides(fill = guide_legend(title = "Starting gene conversion\nrate per site")) +
+#		guides(fill = guide_legend(title = "Starting gene conversion\nrate per site")) +
 	     
 		theme(panel.grid.major=element_blank(),
     	           panel.grid.minor=element_blank(),
     	  	   panel.background=element_blank(),
     	   	   axis.line = element_line(color="black"),
+    	   	   
+    	   	   strip.text = element_text(face = "bold", size = rel(2.5)),
+       	   	   panel.spacing = unit(2, "lines"),    	   	   
     	   	   
 	    	   plot.title=element_text(size=40, hjust=0.5, face="bold"),
 	    	   plot.caption=element_text(size=20, hjust=0.5),
@@ -138,27 +151,27 @@ plot_mean_GC_name <- paste("Figure_mean_fitness_GC_", h_coefficient, "_WithEvol.
 ggsave(filename = plot_mean_GC_name,
 	plot = Plot_mean_GC,
 	dpi = 300,
-	width = 14,
+	width = 24,
 	height = 12)
 
 plot_variance_GC_name <- paste("Figure_variance_fitness_GC_", h_coefficient, "_WithEvol.png", sep="")
 ggsave(filename = plot_variance_GC_name,
 	plot = Plot_variance_GC,
 	dpi = 300,
-	width = 14,
+	width = 24,
 	height = 12)
 
 plot_evolving_GC_name <- paste("Figure_evolving_GC_", h_coefficient, "_WithEvol.png", sep="")
 ggsave(filename = plot_evolving_GC_name,
 	plot = Plot_evol_GC,
 	dpi = 300,
-	width = 14,
+	width = 24,
 	height = 12)
 
-# plot_all_name <- paste("Figure_all_", h_coefficient, "_WithEvol.png", sep="")
-# ggsave(filename = plot_all_name,
-	# plot = Plot_all,
-	# dpi = 300,
-	# width = 14,
-	# height = 12)
+plot_all_name <- paste("Figure_all_", h_coefficient, "_WithEvol.png", sep="")
+ggsave(filename = plot_all_name,
+	plot = Plot_all,
+	dpi = 300,
+	width = 24,
+	height = 24)
 }
